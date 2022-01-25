93f2da1e01e9ba226bf29c6f5dfa423d70013956
Valerio Maggio <leriomaggio@users.noreply.github.com>
2022-01-25 10:56:05 +0000

    Updated functions for mulitple choices
    Multiple Choices now include the possibility to handle the "Other" category as a separate
    choice. process_multiple_choice function now returns
    data with the new Other category (named as specified in input parameter)
    along with the list of other choices found in data.

--- utils.py
+++ utils.py
@@ -8,10 +8,13 @@ from functools import partial
 
 def map_multiple_choices(entries: str, entries_map: dict[str, int]) -> ArrayLike:
     """Map each sample to their list of motivations - spiking a one whenever that motivation was specified"""
-    values = np.zeros(shape=len(entries_map), dtype=int)
+    values = np.zeros(shape=len(entries_map) + 1, dtype=int)
     for choice in entries_map.keys():
         if choice in entries:
             values[entries_map[choice]] = 1
+    entries_purged = quick_purge_presets(entries, entries_map)
+    if len(entries_purged):
+        values[len(entries_map)] = 1
     return values
 
 
@@ -23,14 +26,16 @@ def quick_purge_presets(mm, preset_choices):
 
 
 def process_multiple_choices(
-    data: pd.Series, choices: list[str]
-) -> tuple[tuple[str], ArrayLike]:
+    data: pd.Series,
+    choices: list[str],
+    other_cat_name: str = "Other",
+) -> tuple[tuple[str], tuple[str], ArrayLike]:
     """"""
     quick_purge = partial(quick_purge_presets, preset_choices=choices)
     extra_choices = set(
         filter(lambda m: len(m), map(quick_purge, data.values.tolist()))
     )
-    all_choices = choices + tuple(extra_choices)
-    values_map = {k.strip(): i for i, k in enumerate(all_choices)}
+    all_choices = choices + (other_cat_name,)
+    values_map = {k.strip(): i for i, k in enumerate(choices)}
     map_func = partial(map_multiple_choices, entries_map=values_map)
-    return tuple(values_map.keys()), np.vstack(data.map(map_func)).T
+    return all_choices, extra_choices, np.vstack(data.map(map_func)).T
